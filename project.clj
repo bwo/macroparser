9cde121d6fec7063b6362f0d3c517063bf41327c
ben wolfson <wolfson@gmail.com>
2012-12-25 13:16:49 -0800

    update project.clj, though still requires local copy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject macroparser "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [the/parsatron "0.0.1"]])
+                 [the/parsatron "0.0.3"]])
