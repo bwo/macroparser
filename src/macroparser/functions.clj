ab72637a147e618ef66a240891cd10bfa53ac184
ben wolfson <wolfson@gmail.com>
2013-01-12 21:35:08 -0800

    fix functions.clj import; add fn and fn*

--- src/macroparser/functions.clj
+++ src/macroparser/functions.clj
@@ -1,6 +1,7 @@
 (ns macroparser.functions
-  (:require [macroparser.bindings :as bindings])
-  (:refer-clojure :exclude [vector map symbol])
+  (:require [macroparser.bindings :as bindings]
+            [clojure.core :as clj])
+  (:refer-clojure :exclude [vector map symbol char keyword list])
   (:use [the.parsatron :exclude [string]])
   (:use [macroparser.parsers]))
 
@@ -9,15 +10,30 @@
   (parseq->map (named :params (vector (bindings/vector-binding)))
                (named :body (many (expression)))))
 
+(defparser arities []
+  (named :arities
+         (either (lift clj/list (params-and-body))
+                 (>>1 (many1 (list (params-and-body))) (eof)))))
+
 (defn defn-parser []
   (parseq->map
    (named :type (symbol 'defn))
    (named :name (symbol))
    (named :docstring (maybe (string)))
    (named :attr-map (maybe (map)))
-   (named :arities
-          (either (lift clj/list (params-and-body))
-                  (>>1 (many1 (list (params-and-body))) (eof))))))
+   (arities)))
+
+(defn fn-parser []
+  (parseq->map
+   (named :type (symbols 'fn* 'fn))
+   (named :name (maybe (symbol)))
+   (arities)))
+
+(defn function-parser []
+  (caseparse-noconsume (symbols 'fn 'defn 'fn*)
+                       {'fn (fn-parser)
+                        'fn* (fn-parser)
+                        'defn (defn-parser)}))
 
 (defn unparse-arities [arities]
   (if (= 1 (count arities))
@@ -34,3 +50,13 @@
                  (:attr-map m)
                  (unparse-arities (:arities m)))))
 
+(defn unparse-fn [m]
+  (remove nil?
+          (list* (:type m)
+                 (:name m)
+                 (unparse-arities (:arities m)))))
+
+(defn unparse-function [m]
+  ((case (:type m)
+     (fn fn*) unparse-fn
+     defn unparse-defn) m))
