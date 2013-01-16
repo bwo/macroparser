79e9120c103a3ffb20ee3786227bdfdd89359e00
ben wolfson <wolfson@gmail.com>
2013-01-15 22:07:21 -0800

    description, url

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject macroparser "0.0.1"
-  :description "FIXME: write description"
+  :description "Tools for simplifying the writing of complex macros"
+  :url "https://bitbucket.org/kenko/macroparser"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [the/parsatron "0.0.4"]])
