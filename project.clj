fe12a467d43916142bb539430f5948bdfe6db34f
ben wolfson <wolfson@gmail.com>
2013-01-15 21:58:35 -0800

    parsatron version update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject macroparser "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [the/parsatron "0.0.3"]])
+                 [the/parsatron "0.0.4"]])
