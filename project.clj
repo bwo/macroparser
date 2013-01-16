b955be8c2b9d53732314ca6b9b1e3ab9b2b480a7
ben wolfson <wolfson@gmail.com>
2013-01-15 21:58:57 -0800

    macroparser version decrement

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject macroparser "1.0.0-SNAPSHOT"
+(defproject macroparser "0.0.1"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [the/parsatron "0.0.4"]])
