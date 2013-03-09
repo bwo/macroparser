8b8709458c77bd94dd8111c10fdd1b5d4fd5cfc9
ben wolfson <wolfson@gmail.com>
2013-03-08 17:21:51 -0800

    foo

--- src/macroparser/functions.clj
+++ src/macroparser/functions.clj
@@ -50,7 +50,9 @@
                         'defn (defn-parser)}))
 
 (defn unparse-arity [a body]
-  (clj/list (bindings/unparse-bindings a) body))
+  (if (seq? body)
+    (clj/list* (bindings/unparse-bindings a) body)
+    (clj/list (bindings/unparse-bindings a) body)))
 
 (defn unparse-arities [arities]
   (if (= 1 (count arities))
